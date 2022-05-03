5830c4c9026f95d02f6d68bda5d1ca74d72b15d9
Oliver <oliver.bustamante@pucp.edu.pe>
2022-05-02 23:39:57 -0500

    Crud Sede y Funciones

--- src/main/java/pe/edu/pucp/teleticket/controller/AdminSedesController.java
+++ src/main/java/pe/edu/pucp/teleticket/controller/AdminSedesController.java
@@ -5,16 +5,19 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import pe.edu.pucp.teleticket.entity.Fotossede;
 import pe.edu.pucp.teleticket.entity.Sala;
 import pe.edu.pucp.teleticket.entity.Sede;
 import pe.edu.pucp.teleticket.repository.FotoSedeRepository;
 import pe.edu.pucp.teleticket.repository.SalaRepository;
 import pe.edu.pucp.teleticket.repository.SedeRepository;
 
+import javax.validation.Valid;
+import java.io.IOException;
 import java.util.List;
 import java.util.Optional;
 
@@ -53,6 +56,71 @@ public class AdminSedesController {
         return "admin/sedes/lista";
     }
 
+    @GetMapping("/nueva")
+    public String crearSede(@ModelAttribute("sede") Sede sede){
+        return "admin/sedes/form";
+    }
+
+    @GetMapping("/gestion/{idPath}/editar")
+    public String editarSede(@ModelAttribute("sede") Sede sede,@PathVariable("idPath") String idSedeString, Model model){
+        Integer idSede;
+        try{
+            idSede = Integer.parseInt(idSedeString);
+        } catch (Exception e){
+            return "redirect:/admin/sedes";
+        }
+        if(idSede<1){return "redirect:/admin/sedes";}
+        Optional<Sede> optionalSede = sedeRepository.findById(idSede);
+        if(optionalSede.isEmpty()){return "redirect:/admin/sedes";}
+        sede = optionalSede.get();
+        model.addAttribute("sede", sede);
+        return "admin/sedes/form";
+    }
+    @PostMapping("/guardar")
+    public String guardarSede(@ModelAttribute("sede") @Valid Sede sede, BindingResult bindingResult, @RequestParam("foto") MultipartFile foto , Model model){
+        if(foto.isEmpty()){
+            model.addAttribute("msg","Debe subir un archivo");
+            return "admin/sedes/form";
+        }
+        String fotoNombre = foto.getOriginalFilename();
+        if(fotoNombre.contains("..")){
+            model.addAttribute("msg","No se permiten '..' en el archivo");
+            return "admin/sedes/form";
+        }
+        if(bindingResult.hasErrors()){
+            return "admin/sedes/form";
+        }
+        Fotossede fotosede = new Fotossede();
+        Sede sedeCreada = sedeRepository.save(sede);
+        try{
+            fotosede.setFoto(foto.getBytes());
+            fotosede.setIdsedes(sedeCreada.getId());
+            fotoSedeRepository.save(fotosede);
+        }catch (IOException e){
+            e.printStackTrace();
+            model.addAttribute("msg", "Ocurrió un error al subir el archivo");
+            return "admin/sedes/form";
+        }
+        sede.setFotoprincipal(fotosede.getId());
+        sedeRepository.save(sede);
+
+
+        sede.setFotoprincipal(fotosede.getId());
+
+        return "redirect:/admin/sedes/gestion/"+sede.getId();
+    }
+
+    @PostMapping("/actualizar")
+    public String actualizarSede(@ModelAttribute("sede") @Valid Sede sede, BindingResult bindingResult , Model model){
+        if(bindingResult.hasErrors()){
+            return "admin/sedes/form";
+        }
+        sedeRepository.save(sede);
+        return "redirect:/admin/sedes/gestion/"+sede.getId();
+    }
+
+
+
     @GetMapping("/gestion/{idPath}")
     public String gestionSede(Model model, @PathVariable("idPath") String idPath, @RequestParam("pag") Optional<Integer> pag){
         int id=0;
@@ -87,17 +155,56 @@ public class AdminSedesController {
     }
 
     @GetMapping("/gestion/{idPath}/nuevasala")
-    public String nuevaSala(@PathVariable("idPath") String idPath, Model model){
-        int id=0;
+    public String nuevaSala(@PathVariable("idPath") String idSedeString, @ModelAttribute("sala") Sala sala){
+        int idSede=0;
         try{
-            id= Integer.parseInt(idPath);
+            idSede= Integer.parseInt(idSedeString);
         } catch (Exception e){
             return "redirect:/admin/sedes";
         }
-        Optional<Sede> optionalSede =sedeRepository.findById(id);
+        Optional<Sede> optionalSede =sedeRepository.findById(idSede);
         if(optionalSede.isEmpty()){return "redirect:/admin/sedes";}
-        model.addAttribute("id",id);
-
+        sala.setSede(new Sede());
+        sala.getSede().setId(idSede);
         return "/admin/sedes/salas/form";
     }
+
+    @GetMapping("/gestion/editarsala")
+    public String editarSala( @RequestParam("id") String idSalaString, @ModelAttribute("sala") Sala sala, Model model){
+        int idSala=0;
+        try{
+            idSala= Integer.parseInt(idSalaString);
+        } catch (Exception e){
+            return "redirect:/admin/sedes";
+        }
+
+        Optional<Sala> optionalSala = salaRepository.findById(idSala);
+        if(optionalSala.isEmpty()){return "redirect:/admin/sedes";}
+        sala = optionalSala.get();
+        model.addAttribute("sala", sala);
+        model.addAttribute("idSede",sala.getSede().getId());
+        return "/admin/sedes/salas/form";
+    }
+
+    @PostMapping("/gestion/sala/guardar")
+    public String guardarSala(@ModelAttribute("sala") @Valid Sala sala, BindingResult bindingResult , RedirectAttributes redirectAttributes){
+
+        if(bindingResult.hasErrors()){
+            return "/admin/sedes/salas/form";
+        }
+
+        String msg;
+        if(sala.getId()==0){
+            msg="Sala creada exitosamente";
+        } else {
+            msg="Sala editada exitosamente";
+        }
+        redirectAttributes.addFlashAttribute("msg",msg);
+        salaRepository.save(sala);
+        return "redirect:/admin/sedes/gestion/" + sala.getSede().getId();
+    }
+
+
+
+
 }
--- src/main/java/pe/edu/pucp/teleticket/entity/Sede.java
+++ src/main/java/pe/edu/pucp/teleticket/entity/Sede.java
@@ -2,10 +2,12 @@ package pe.edu.pucp.teleticket.entity;
 
 import lombok.Getter;
 import lombok.Setter;
+import org.hibernate.validator.constraints.Length;
 import org.springframework.beans.factory.annotation.Autowired;
 import pe.edu.pucp.teleticket.repository.FotoSedeRepository;
 
 import javax.persistence.*;
+import javax.validation.constraints.*;
 import java.util.LinkedHashSet;
 import java.util.List;
 import java.util.Set;
@@ -17,24 +19,33 @@ import java.util.Set;
 
 public class Sede {
 
-
-
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "idsedes", nullable = false)
     private int id;
 
+    @NotEmpty(message = "Tiene que llenar el nombre")
+    @Length(message = "No puede exceder de los 70 caracteres", max = 70)
     @Column(name = "nombre", nullable = false, length = 70)
     private String nombre;
 
+    @NotNull(message = "Tiene que llenar la latitud")
+    @Max(message = "No puede ser mayor a 90°", value = 90)
+    @Min(message = "No puede ser menor a -90°", value = -90)
     @Column(name = "latitud", nullable = false)
     private Double latitud;
 
+    @Max(message = "No puede ser mayor que 180°", value = 180)
+    @Min(message = "No puede ser menor que -180°", value = -180)
+    @NotNull(message = "Tiene que llenar la longitud")
     @Column(name = "longitud", nullable = false)
     private Double longitud;
 
+    @NotBlank(message = "Tiene que llenar la dirección")
     @Column(name = "direccion", nullable = false)
     private String direccion;
 
+    @NotNull(message = "Tiene que seleccionar un estado")
     @Lob
     @Column(name = "estado", nullable = false)
     private String estado;
