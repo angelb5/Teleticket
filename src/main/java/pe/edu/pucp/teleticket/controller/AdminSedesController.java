5e6e87d44516d8f4aeb1ea49b079155595703140
Oliver <oliver.bustamante@pucp.edu.pe>
2022-05-03 00:56:41 -0500

    Crud Sedes finalizado, verificaciones e imagenes funcionando

--- src/main/java/pe/edu/pucp/teleticket/controller/AdminSedesController.java
+++ src/main/java/pe/edu/pucp/teleticket/controller/AdminSedesController.java
@@ -36,67 +36,72 @@ public class AdminSedesController {
     @Autowired
     FotoSedeRepository fotoSedeRepository;
 
-    @GetMapping({"/","","/lista"})
-    public String listarSedes(Model model, @RequestParam("pag") Optional<Integer> pag){
-        int pagina = pag.isEmpty()? 1 : pag.get();
-        pagina = pagina<1? 1 : pagina;
-        int paginas = (int) Math.ceil((float)sedeRepository.count()/sedesPaginas);
-        pagina = pagina>paginas? paginas : pagina;
+    @GetMapping({"/", "", "/lista"})
+    public String listarSedes(Model model, @RequestParam("pag") Optional<Integer> pag) {
+        int pagina = pag.isEmpty() ? 1 : pag.get();
+        pagina = pagina < 1 ? 1 : pagina;
+        int paginas = (int) Math.ceil((float) sedeRepository.count() / sedesPaginas);
+        pagina = pagina > paginas ? paginas : pagina;
         Pageable lista;
-        if(pagina==0){
-            lista= PageRequest.of(0, sedesPaginas);
+        if (pagina == 0) {
+            lista = PageRequest.of(0, sedesPaginas);
         } else {
-            lista= PageRequest.of(pagina-1, sedesPaginas);
+            lista = PageRequest.of(pagina - 1, sedesPaginas);
 
         }
         List<Sede> listaSedes = sedeRepository.findAllByOrderByIdAsc(lista);
-        model.addAttribute("listaSedes",listaSedes);
+        model.addAttribute("listaSedes", listaSedes);
         model.addAttribute("pag", pagina);
         model.addAttribute("paginas", paginas);
         return "admin/sedes/lista";
     }
 
     @GetMapping("/nueva")
-    public String crearSede(@ModelAttribute("sede") Sede sede){
+    public String crearSede(@ModelAttribute("sede") Sede sede) {
         return "admin/sedes/form";
     }
 
     @GetMapping("/gestion/{idPath}/editar")
-    public String editarSede(@ModelAttribute("sede") Sede sede,@PathVariable("idPath") String idSedeString, Model model){
+    public String editarSede(@ModelAttribute("sede") Sede sede, @PathVariable("idPath") String idSedeString, Model model) {
         Integer idSede;
-        try{
+        try {
             idSede = Integer.parseInt(idSedeString);
-        } catch (Exception e){
+        } catch (Exception e) {
             return "redirect:/admin/sedes";
         }
-        if(idSede<1){return "redirect:/admin/sedes";}
+        if (idSede < 1) {
+            return "redirect:/admin/sedes";
+        }
         Optional<Sede> optionalSede = sedeRepository.findById(idSede);
-        if(optionalSede.isEmpty()){return "redirect:/admin/sedes";}
+        if (optionalSede.isEmpty()) {
+            return "redirect:/admin/sedes";
+        }
         sede = optionalSede.get();
         model.addAttribute("sede", sede);
         return "admin/sedes/form";
     }
+
     @PostMapping("/guardar")
-    public String guardarSede(@ModelAttribute("sede") @Valid Sede sede, BindingResult bindingResult, @RequestParam("foto") MultipartFile foto , Model model){
-        if(foto.isEmpty()){
-            model.addAttribute("msg","Debe subir un archivo");
+    public String guardarSede(@ModelAttribute("sede") @Valid Sede sede, BindingResult bindingResult, @RequestParam("foto") MultipartFile foto, Model model) {
+        if (foto.isEmpty()) {
+            model.addAttribute("msg", "Debe subir un archivo");
             return "admin/sedes/form";
         }
         String fotoNombre = foto.getOriginalFilename();
-        if(fotoNombre.contains("..")){
-            model.addAttribute("msg","No se permiten '..' en el archivo");
+        if (fotoNombre.contains("..")) {
+            model.addAttribute("msg", "No se permiten '..' en el archivo");
             return "admin/sedes/form";
         }
-        if(bindingResult.hasErrors()){
+        if (bindingResult.hasErrors()) {
             return "admin/sedes/form";
         }
         Fotossede fotosede = new Fotossede();
         Sede sedeCreada = sedeRepository.save(sede);
-        try{
+        try {
             fotosede.setFoto(foto.getBytes());
             fotosede.setIdsedes(sedeCreada.getId());
             fotoSedeRepository.save(fotosede);
-        }catch (IOException e){
+        } catch (IOException e) {
             e.printStackTrace();
             model.addAttribute("msg", "Ocurrió un error al subir el archivo");
             return "admin/sedes/form";
@@ -107,104 +112,195 @@ public class AdminSedesController {
 
         sede.setFotoprincipal(fotosede.getId());
 
-        return "redirect:/admin/sedes/gestion/"+sede.getId();
+        return "redirect:/admin/sedes/gestion/" + sede.getId();
     }
 
     @PostMapping("/actualizar")
-    public String actualizarSede(@ModelAttribute("sede") @Valid Sede sede, BindingResult bindingResult , Model model){
-        if(bindingResult.hasErrors()){
+    public String actualizarSede(@ModelAttribute("sede") @Valid Sede sede, BindingResult bindingResult, Model model) {
+        if (bindingResult.hasErrors()) {
             return "admin/sedes/form";
         }
         sedeRepository.save(sede);
-        return "redirect:/admin/sedes/gestion/"+sede.getId();
+        return "redirect:/admin/sedes/gestion/" + sede.getId();
     }
 
 
-
     @GetMapping("/gestion/{idPath}")
-    public String gestionSede(Model model, @PathVariable("idPath") String idPath, @RequestParam("pag") Optional<Integer> pag){
-        int id=0;
-        try{
-            id= Integer.parseInt(idPath);
-        } catch (Exception e){
+    public String gestionSede(Model model, @PathVariable("idPath") String idPath, @RequestParam("pag") Optional<Integer> pag) {
+        int id = 0;
+        try {
+            id = Integer.parseInt(idPath);
+        } catch (Exception e) {
             return "redirect:/admin/sedes";
         }
-        Optional<Sede> optionalSede =sedeRepository.findById(id);
-        if(optionalSede.isEmpty()){return "redirect:/admin/sedes";}
+        Optional<Sede> optionalSede = sedeRepository.findById(id);
+        if (optionalSede.isEmpty()) {
+            return "redirect:/admin/sedes";
+        }
 
-        int pagina = pag.isEmpty()? 1 : pag.get();
-        pagina = pagina<1? 1 : pagina;
+        int pagina = pag.isEmpty() ? 1 : pag.get();
+        pagina = pagina < 1 ? 1 : pagina;
         Sede sede = new Sede();
         sede.setId(id);
-        int paginas = (int) Math.ceil((float)salaRepository.countAllBySede(sede)/salasPaginas);
-        pagina = pagina>paginas? paginas : pagina;
+        int paginas = (int) Math.ceil((float) salaRepository.countAllBySede(sede) / salasPaginas);
+        pagina = pagina > paginas ? paginas : pagina;
         Pageable lista;
-        if(pagina==0){
-            lista= PageRequest.of(0, salasPaginas);
+        if (pagina == 0) {
+            lista = PageRequest.of(0, salasPaginas);
         } else {
-            lista= PageRequest.of(pagina-1, salasPaginas);
+            lista = PageRequest.of(pagina - 1, salasPaginas);
 
         }
 
         model.addAttribute("fotos", fotoSedeRepository.findAllIdByIdSedes(id));
         model.addAttribute("pag", pagina);
         model.addAttribute("paginas", paginas);
-        model.addAttribute("sede",optionalSede.get());
-        model.addAttribute("salas", salaRepository.findAllBySede(sede,lista));
+        model.addAttribute("sede", optionalSede.get());
+        model.addAttribute("salas", salaRepository.findAllBySede(sede, lista));
         return "/admin/sedes/gestionsede";
     }
 
     @GetMapping("/gestion/{idPath}/nuevasala")
-    public String nuevaSala(@PathVariable("idPath") String idSedeString, @ModelAttribute("sala") Sala sala){
-        int idSede=0;
-        try{
-            idSede= Integer.parseInt(idSedeString);
-        } catch (Exception e){
+    public String nuevaSala(@PathVariable("idPath") String idSedeString, @ModelAttribute("sala") Sala sala) {
+        int idSede = 0;
+        try {
+            idSede = Integer.parseInt(idSedeString);
+        } catch (Exception e) {
             return "redirect:/admin/sedes";
         }
-        Optional<Sede> optionalSede =sedeRepository.findById(idSede);
-        if(optionalSede.isEmpty()){return "redirect:/admin/sedes";}
+        Optional<Sede> optionalSede = sedeRepository.findById(idSede);
+        if (optionalSede.isEmpty()) {
+            return "redirect:/admin/sedes";
+        }
         sala.setSede(new Sede());
         sala.getSede().setId(idSede);
         return "/admin/sedes/salas/form";
     }
 
     @GetMapping("/gestion/editarsala")
-    public String editarSala( @RequestParam("id") String idSalaString, @ModelAttribute("sala") Sala sala, Model model){
-        int idSala=0;
-        try{
-            idSala= Integer.parseInt(idSalaString);
-        } catch (Exception e){
+    public String editarSala(@RequestParam("id") String idSalaString, @ModelAttribute("sala") Sala sala, Model model) {
+        int idSala = 0;
+        try {
+            idSala = Integer.parseInt(idSalaString);
+        } catch (Exception e) {
             return "redirect:/admin/sedes";
         }
 
         Optional<Sala> optionalSala = salaRepository.findById(idSala);
-        if(optionalSala.isEmpty()){return "redirect:/admin/sedes";}
+        if (optionalSala.isEmpty()) {
+            return "redirect:/admin/sedes";
+        }
         sala = optionalSala.get();
         model.addAttribute("sala", sala);
-        model.addAttribute("idSede",sala.getSede().getId());
+        model.addAttribute("idSede", sala.getSede().getId());
         return "/admin/sedes/salas/form";
     }
 
     @PostMapping("/gestion/sala/guardar")
-    public String guardarSala(@ModelAttribute("sala") @Valid Sala sala, BindingResult bindingResult , RedirectAttributes redirectAttributes){
+    public String guardarSala(@ModelAttribute("sala") @Valid Sala sala, BindingResult bindingResult, RedirectAttributes redirectAttributes) {
 
-        if(bindingResult.hasErrors()){
+        if (bindingResult.hasErrors()) {
             return "/admin/sedes/salas/form";
         }
 
         String msg;
-        if(sala.getId()==0){
-            msg="Sala creada exitosamente";
+        if (sala.getId() == 0) {
+            msg = "Sala creada exitosamente";
         } else {
-            msg="Sala editada exitosamente";
+            msg = "Sala editada exitosamente";
         }
-        redirectAttributes.addFlashAttribute("msg",msg);
+        redirectAttributes.addFlashAttribute("msg", msg);
         salaRepository.save(sala);
         return "redirect:/admin/sedes/gestion/" + sala.getSede().getId();
     }
 
+    @PostMapping("/gestion/{idSedeString}/imagenprincipal")
+    public String imagenPrincipal(@PathVariable("idSedeString") String idSedeString, @RequestParam("fotoid") String fotoidString) {
+        int sedeId;
+        int fotoid;
+        try {
+            sedeId = Integer.parseInt(idSedeString);
+            fotoid = Integer.parseInt(fotoidString);
 
+        } catch (Exception e) {
+            return "redirect:/admin/sedes";
+        }
+        Optional<Sede> optionalSede = sedeRepository.findById(sedeId);
+        Optional<Fotossede> optionalFotossede = fotoSedeRepository.findById(fotoid);
+        if (optionalFotossede.isEmpty() || optionalSede.isEmpty() || (optionalFotossede.get().getIdsedes() != sedeId)) {
+            return "redirect:/admin/sedes";
+        }
+
+        Sede sede = optionalSede.get();
+        sede.setFotoprincipal(fotoid);
+        sedeRepository.save(sede);
+        return "redirect:/admin/sedes/gestion/" + sedeId;
+    }
+
+    @PostMapping("/gestion/imagenborrar")
+    public String borrarImagen(@RequestParam("fotoid") String fotoidString) {
+        int fotoid;
+        try {
+            fotoid = Integer.parseInt(fotoidString);
+        } catch (Exception e) {
+            return "redirect:/admin/sedes";
+        }
+        Optional<Fotossede> optionalFotossede = fotoSedeRepository.findById(fotoid);
+        if (optionalFotossede.isEmpty()) {
+            return "redirect:/admin/sedes";
+        }
+        Fotossede fotossede = optionalFotossede.get();
+        Optional<Sede> optionalSede = sedeRepository.findById(fotossede.getIdsedes());
+        if (optionalSede.isEmpty() || (optionalSede.get().getFotoprincipal() == fotoid)) {
+            return "redirect:/admin/sedes";
+        }
+
+
+        fotoSedeRepository.deleteById(fotoid);
+        return "redirect:/admin/sedes/gestion/" + optionalFotossede.get().getIdsedes();
+    }
+
+    @PostMapping("/gestion/{idSedeString}/agregarimagen")
+    public String agregarImagen(@PathVariable("idSedeString") String idSedeString, @RequestParam("foto") MultipartFile foto, RedirectAttributes redirectAttributes) {
+        int sedeId;
+        try {
+            sedeId = Integer.parseInt(idSedeString);
+        } catch (Exception e) {
+            return "redirect:/admin/sedes";
+        }
+        Optional<Sede> optionalSede = sedeRepository.findById(sedeId);
+        if (optionalSede.isEmpty()) {
+            return "redirect:/admin/sedes";
+        }
+        long cantidad= fotoSedeRepository.findAllIdByIdSedes(sedeId).size();
+        if(cantidad>3){
+            return "redirect:/admin/sedes/gestion/" + sedeId;
+
+        }
+        if (foto.isEmpty()) {
+            redirectAttributes.addFlashAttribute("msg", "Debe subir un archivo");
+            return "redirect:/admin/sedes/gestion/" + sedeId;
+        }
+        String fotoNombre = foto.getOriginalFilename();
+        if (fotoNombre.contains("..")) {
+            redirectAttributes.addFlashAttribute("msg", "No se permiten '..' en el archivo");
+            return "redirect:/admin/sedes/gestion/" + sedeId;
+        }
+
+        Fotossede fotossede = new Fotossede();
+        try {
+            fotossede.setFoto(foto.getBytes());
+            fotossede.setIdsedes(sedeId);
+        } catch (IOException e){
+            e.printStackTrace();
+            redirectAttributes.addFlashAttribute("msg", "Hubo un error al cargar el archivp");
+            return "redirect:/admin/sedes/gestion/" + sedeId;
+        }
+        fotoSedeRepository.save(fotossede);
+
+
+        return "redirect:/admin/sedes/gestion/" + sedeId;
+    }
 
 
 }
