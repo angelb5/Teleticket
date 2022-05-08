d75ebe2f31b390d0c88070d8f8b7c919e13e52a0
Oliver <oliver.bustamante@pucp.edu.pe>
2022-05-08 02:11:20 -0500

    Listado de operadores

--- src/main/java/pe/edu/pucp/teleticket/controller/AdminOperadoresController.java
+++ src/main/java/pe/edu/pucp/teleticket/controller/AdminOperadoresController.java
@@ -1,14 +1,55 @@
 package pe.edu.pucp.teleticket.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import pe.edu.pucp.teleticket.repository.OperadorRepository;
+
+import java.util.Optional;
 
 @Controller
 @RequestMapping("/admin/operadores")
 public class AdminOperadoresController {
+
+    private final int operadoresPaginas=8;
+
+    @Autowired
+    OperadorRepository operadorRepository;
+
+
     @GetMapping({"/","","/lista"})
-    public String listarSedes(){
+    public String listarSedes(@ModelAttribute("nombreOperador") String nombreOperador, Model model,
+                              @RequestParam("pag") Optional<String> pagString){
+        int pagina;
+        try {
+            if (pagString.isEmpty() || pagString.get().isEmpty()) {
+                pagina = 0;
+            } else {
+                pagina = Integer.parseInt(pagString.get());
+            }
+            if(nombreOperador==null){
+                nombreOperador="";
+            }
+        } catch (Exception e) {
+            return "redirect:/admin/clientes";
+        }
+        pagina = pagina < 1 ? 1 : pagina;
+        int paginas = (int) Math.ceil((float) operadorRepository.contarListaOperadores(nombreOperador) / operadoresPaginas);
+        pagina = pagina > paginas ? paginas : pagina;
+        Pageable lista = PageRequest.of(pagina - 1, operadoresPaginas);
+
+        model.addAttribute("operadores", operadorRepository.listarOperadores(nombreOperador, lista));
+        model.addAttribute("pag", pagina);
+        model.addAttribute("paginas", paginas);
+        model.addAttribute("ruta", "/admin/operadores");
+
+
         return "admin/operadores/lista";
     }
 }
--- src/main/java/pe/edu/pucp/teleticket/repository/OperadorRepository.java
+++ src/main/java/pe/edu/pucp/teleticket/repository/OperadorRepository.java
@@ -1,10 +1,23 @@
 package pe.edu.pucp.teleticket.repository;
 
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import pe.edu.pucp.teleticket.entity.Operador;
+
+
+import java.util.List;
 
 @Repository
 public interface OperadorRepository extends JpaRepository<Operador, Integer> {
     public Operador findByCorreo(String correo);
+
+    @Query(nativeQuery = true,value = "select * from operadores o where LOWER(o.nombre) like %?1% order by nombre asc",
+    countQuery = "select count(*) from operadores o where LOWER(o.nombre) like %?1% order by nombre asc")
+    public List<Operador> listarOperadores(String nombre, Pageable paginado);
+
+    @Query(nativeQuery = true, value = "select count(*) from operadores o where LOWER(o.nombre) like %?1% ")
+     public long contarListaOperadores(String nombre);
+
 }
