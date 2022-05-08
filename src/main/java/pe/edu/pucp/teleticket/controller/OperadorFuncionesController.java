185112131b525221ea6fcaa01de3e29c231a1766
Angel <a20190192@pucp.edu.pe>
2022-05-08 04:01:45 -0500

    Form Registrar, cambios crud funciones

--- src/main/java/pe/edu/pucp/teleticket/controller/OperadorFuncionesController.java
+++ src/main/java/pe/edu/pucp/teleticket/controller/OperadorFuncionesController.java
@@ -61,65 +61,72 @@ public class OperadorFuncionesController {
     }
 
     @PostMapping("/registrar")
-    public String registaraFuncion(@ModelAttribute("funcion") @Valid Funcion funcion, BindingResult bindingResult,
+    public String registarFuncion(@ModelAttribute("funcion") @Valid Funcion funcion, BindingResult bindingResult,
                                    RedirectAttributes attr, Model model){
         int idobra = funcion.getObra().getId();
         Optional<Obra> optionalObra = obraRepository.findById(idobra);
-        List<Sede> sedeList = sedeRepository.findAll();
+
         if(idobra==0 || optionalObra.isEmpty() ) {
             return "redirect:/operador/";
-        }else if(bindingResult.hasErrors()){
+        }
+
+        List<Sede> sedeList = sedeRepository.findAll();
+
+        boolean horaHasErrors = false;
+        boolean fechaHasErrors = false;
+        boolean salaHasErrors = false;
+
+        if(!bindingResult.hasFieldErrors("inicio")){
+            LocalTime minTime = LocalTime.parse( "11:00:00" );
+            LocalTime maxTime = LocalTime.parse( "23:00:00" );
+            if(funcion.getInicio().isAfter(maxTime) || funcion.getInicio().isBefore(minTime)){
+                FieldError fHoraerror = new FieldError("inicio", "inicio", "La hora de inicio debe estar entre 11:00 y 23:00");
+                bindingResult.addError(fHoraerror);
+                horaHasErrors = true;
+            }
+        }
+
+        if(!bindingResult.hasFieldErrors("fecha")){
+            LocalDate minDate = LocalDate.now().plusDays(1);
+            LocalDate maxDate = LocalDate.now().plusMonths(4);
+            if(funcion.getFecha().isAfter(maxDate) || funcion.getFecha().isBefore(minDate)){
+                FieldError fFechaerror = new FieldError("fecha", "fecha", "La fecha debe estar entre " +minDate+" y "+maxDate);
+                bindingResult.addError(fFechaerror);
+                fechaHasErrors=true;
+            }
+        }
+
+        if(!bindingResult.hasFieldErrors("sala")){
+            Optional<Sala> optionalSala = salaRepository.findById(funcion.getSala().getId());
+            if(optionalSala.map(sala -> sala.getEstado().equals("No disponible")).orElse(true)){
+                FieldError fSalaError = new FieldError("sala", "sala", "Debe eligir una sala válida");
+                bindingResult.addError(fSalaError);
+                salaHasErrors = true;
+            }else{
+                if(funcion.getStock() > optionalSala.get().getAforo()){
+                    FieldError fAforoError = new FieldError("stock", "stock", "El aforo de la función no puede ser mayor que el aforo de la sala");
+                    bindingResult.addError(fAforoError);
+                    salaHasErrors = true;
+                }
+            }
+        }
+
+        if(bindingResult.hasErrors() || horaHasErrors || fechaHasErrors || salaHasErrors){
             model.addAttribute("sedeList", sedeList);
             return "operador/funciones/nuevaFrm";
         }else{
-            LocalTime minTime = LocalTime.parse( "11:00:00" );
-            LocalTime maxTime = LocalTime.parse( "23:00:00" );
-            boolean horaError = funcion.getInicio().isAfter(maxTime) || funcion.getInicio().isBefore(minTime);
-            if(horaError){
-                FieldError fHoraerror = new FieldError("inicio", "inicio", "La hora de inicio debe estar entre 11:00 y 23:00");
-                bindingResult.addError(fHoraerror);
-            }
-
-            LocalDate minDate = LocalDate.now().plusDays(1);
-            LocalDate maxDate = LocalDate.now().plusMonths(4);
-            boolean fechaError = funcion.getFecha().isAfter(maxDate) || funcion.getFecha().isBefore(minDate);
-            if(fechaError){
-                FieldError fFechaerror = new FieldError("fecha", "fecha", "La fecha debe estar entre " +minDate+" y "+maxDate);
-                bindingResult.addError(fFechaerror);
-            }
-
-            Optional<Sala> optionalSala = salaRepository.findById(funcion.getSala().getId());
-            boolean salaError = optionalSala.map(sala -> sala.getEstado().equals("No disponible")).orElse(true);
-            boolean aforoError = false;
-            if(salaError){
-                FieldError fSalaError = new FieldError("sala", "sala", "Debe eligir una sala válida");
-                bindingResult.addError(fSalaError);
-            }else{
-                aforoError = funcion.getStock() > optionalSala.get().getAforo();
-                if(aforoError){
-                    FieldError fAforoError = new FieldError("stock", "stock", "El aforo de la función no puede ser mayor que el aforo de la sala");
-                    bindingResult.addError(fAforoError);
-                }
-            }
-
-            if(horaError || fechaError || aforoError || salaError){
+            LocalTime fin = funcion.getInicio().plusMinutes(optionalObra.get().getDuracion());
+            funcion.setFin(fin);
+            List<Funcion> funcionesConflictoList = funcionRepository.findFuncionesEnConflicto(funcion.getFecha(),funcion.getInicio(),fin,funcion.getObra().getId(), funcion.getSala().getId());
+            if(funcionesConflictoList.size()>0){
+                model.addAttribute("funcionesConflictoList", funcionesConflictoList);
                 model.addAttribute("sedeList", sedeList);
                 return "operador/funciones/nuevaFrm";
             }else{
-                LocalTime fin = funcion.getInicio().plusMinutes(optionalObra.get().getDuracion());
-                funcion.setFin(fin);
-                List<Funcion> funcionesConflictoList = funcionRepository.findFuncionesEnConflicto(funcion.getFecha(),funcion.getInicio(),fin,funcion.getObra().getId(), funcion.getSala().getId());
-                if(funcionesConflictoList.size()>0){
-                    model.addAttribute("funcionesConflictoList", funcionesConflictoList);
-                    model.addAttribute("sedeList", sedeList);
-                    return "operador/funciones/nuevaFrm";
-                }else{
-                    funcion.setEstado("Activa");
-                    funcionRepository.save(funcion);
-                    attr.addFlashAttribute("msg", "Se ha creado la función con éxito");
-                    return "redirect:/operador/obras/gestion/" + funcion.getObra().getId();
-                }
-
+                funcion.setEstado("Activa");
+                funcionRepository.save(funcion);
+                attr.addFlashAttribute("msg", "Se ha creado la función con éxito");
+                return "redirect:/operador/obras/gestion/" + funcion.getObra().getId();
             }
         }
     }
@@ -130,66 +137,73 @@ public class OperadorFuncionesController {
         int idfuncion = funcion.getId();
         Optional<Funcion> optionalFuncion = funcionRepository.findById(idfuncion);
         if(idfuncion==0 || optionalFuncion.isEmpty() ) {return "redirect:/operador/";}
+
+        int idobra = funcion.getObra().getId();
+        Optional<Obra> optionalObra = obraRepository.findById(idobra);
         Funcion funcionDB = optionalFuncion.get();
         funcion.setSala(funcionDB.getSala());
         funcion.setObra(funcionDB.getObra());
         funcion.setEstado(funcionDB.getEstado());
         funcion.setStock(funcionDB.getStock());
         if(funcion.getEstado().equals("Cancelada") || funcionDB.getEstadoRVC().equals("Realizada")){return "redirect:/operador/";}
+
+        boolean horaHasErrors = false;
+        boolean fechaHasErrors = false;
+        boolean salaHasErrors = false;
+
+        if(!bindingResult.hasFieldErrors("inicio")){
+            LocalTime minTime = LocalTime.parse( "11:00:00" );
+            LocalTime maxTime = LocalTime.parse( "23:00:00" );
+            if(funcion.getInicio().isAfter(maxTime) || funcion.getInicio().isBefore(minTime)){
+                FieldError fHoraerror = new FieldError("inicio", "inicio", "La hora de inicio debe estar entre 11:00 y 23:00");
+                bindingResult.addError(fHoraerror);
+                horaHasErrors = true;
+            }
+        }
+
+        if(!bindingResult.hasFieldErrors("fecha")){
+            LocalDate minDate = LocalDate.now().plusDays(1);
+            LocalDate maxDate = LocalDate.now().plusMonths(4);
+            if(funcion.getFecha().isAfter(maxDate) || funcion.getFecha().isBefore(minDate)){
+                FieldError fFechaerror = new FieldError("fecha", "fecha", "La fecha debe estar entre " +minDate+" y "+maxDate);
+                bindingResult.addError(fFechaerror);
+                fechaHasErrors=true;
+            }
+        }
+
+        if(!bindingResult.hasFieldErrors("sala")){
+            Optional<Sala> optionalSala = salaRepository.findById(funcion.getSala().getId());
+            if(optionalSala.map(sala -> sala.getEstado().equals("No disponible")).orElse(true)){
+                FieldError fSalaError = new FieldError("sala", "sala", "Debe eligir una sala válida");
+                bindingResult.addError(fSalaError);
+                salaHasErrors = true;
+            }else{
+                if(funcion.getStock() > optionalSala.get().getAforo()){
+                    FieldError fAforoError = new FieldError("stock", "stock", "El aforo de la función no puede ser mayor que el aforo de la sala");
+                    bindingResult.addError(fAforoError);
+                    salaHasErrors = true;
+                }
+            }
+        }
+
         if(bindingResult.hasFieldErrors("fecha") || bindingResult.hasFieldErrors("maxreservas") ||
-                bindingResult.hasFieldErrors("inicio") || bindingResult.hasFieldErrors("costo")){
+                bindingResult.hasFieldErrors("inicio") || bindingResult.hasFieldErrors("costo")
+                || horaHasErrors || fechaHasErrors || salaHasErrors ){
             model.addAttribute("funcion", funcion);
             return "operador/funciones/editaFrm";
         }else{
-            Optional<Obra> optionalObra = obraRepository.findById( funcion.getObra().getId());
-            LocalTime minTime = LocalTime.parse( "11:00:00" );
-            LocalTime maxTime = LocalTime.parse( "23:00:00" );
-            boolean horaError = funcion.getInicio().isAfter(maxTime) || funcion.getInicio().isBefore(minTime);
-            if(horaError){
-                FieldError fHoraerror = new FieldError("inicio", "inicio", "La hora de inicio debe estar entre 11:00 y 23:00");
-                bindingResult.addError(fHoraerror);
-            }
-
-            LocalDate minDate = LocalDate.now().plusDays(1);
-            LocalDate maxDate = LocalDate.now().plusMonths(4);
-            boolean fechaError = funcion.getFecha().isAfter(maxDate) || funcion.getFecha().isBefore(minDate);
-            if(fechaError){
-                FieldError fFechaerror = new FieldError("fecha", "fecha", "La fecha debe estar entre " +minDate+" y "+maxDate);
-                bindingResult.addError(fFechaerror);
-            }
-
-            Optional<Sala> optionalSala = salaRepository.findById(funcion.getSala().getId());
-            boolean salaError = optionalSala.map(sala -> sala.getEstado().equals("No disponible")).orElse(true);
-            boolean aforoError = false;
-            if(salaError){
-                FieldError fSalaError = new FieldError("sala", "sala", "Debe eligir una sala válida");
-                bindingResult.addError(fSalaError);
-            }else{
-                aforoError = funcion.getStock() > optionalSala.get().getAforo();
-                if(aforoError){
-                    FieldError fAforoError = new FieldError("stock", "stock", "El aforo de la función no puede ser mayor que el aforo de la sala");
-                    bindingResult.addError(fAforoError);
-                }
-            }
-
-            if(horaError || fechaError || aforoError || salaError){
-                model.addAttribute("funcion", funcion);
-                return "operador/funciones/editaFrm";
-            }else{
                 LocalTime fin = funcion.getInicio().plusMinutes(optionalObra.get().getDuracion());
+                funcion.setFin(fin);
                 List<Funcion> funcionesConflictoList = funcionRepository.findFuncionesEnConflictoId(funcion.getFecha(),funcion.getInicio(),fin,funcion.getObra().getId(), funcion.getSala().getId(), funcion.getId());
                 if(funcionesConflictoList.size()>0){
                     model.addAttribute("funcionesConflictoList", funcionesConflictoList);
                     model.addAttribute("funcion", funcion);
                     return "operador/funciones/editaFrm";
                 }else{
-                    funcion.setFin(fin);
                     funcionRepository.save(funcion);
                     attr.addFlashAttribute("msg", "Se ha modificado la función con éxito");
                     return "redirect:/operador/obras/gestion/" + funcion.getObra().getId();
                 }
-
-            }
         }
     }
 
