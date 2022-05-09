982e84d8983c3b49158a899f0147e151cf86e5ab
Angel <a20190192@pucp.edu.pe>
2022-05-08 21:41:54 -0500

    navbar busqueda

--- src/main/resources/static/js/busqueda.js
+++ src/main/resources/static/js/busqueda.js
@@ -1,9 +1,14 @@
 $(document).ready(function(e){
+    rol = $('.search-panel span#rol').text();
+    buscaren = $('.search-panel span#buscaren').text();
+    $('#busquedaFrm').attr('action', '/'+rol+'/'+buscaren)
+
     $('.search-panel .dropdown-menu').find('a').click(function(e) {
         e.preventDefault();
         var param = $(this).attr("href").replace("#","");
         var concept = $(this).text();
         $('.search-panel span#search_concept').text(concept);
-        $('.input-group #search_param').val(param);
+        $('#busquedaFrm').attr('action', '/'+rol+'/'+param)
     });
+
 });
