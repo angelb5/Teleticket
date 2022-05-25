3db30063252fb6d97e35ba1034381d56cc6a6057
Samantha Sanchez <94878885+SamanthaXimena@users.noreply.github.com>
2022-05-25 16:28:44 -0500

    cambio direccion de miperfil

--- src/main/java/pe/edu/pucp/teleticket/controller/ClienteController.java
+++ src/main/java/pe/edu/pucp/teleticket/controller/ClienteController.java
@@ -22,7 +22,7 @@ import java.util.List;
 import java.util.Locale;
 import java.util.Optional;
 
-@RequestMapping("/cliente")
+@RequestMapping("/")
 @Controller
 public class ClienteController {
 
@@ -35,7 +35,7 @@ public class ClienteController {
     public String miPerfil(Model model, HttpSession session){
         Cliente cliente = (Cliente) session.getAttribute("usuario");
         model.addAttribute("cliente", cliente);
-        return "/cliente/miperfil";
+        return "/miperfil";
     }
 
     @GetMapping("/imagen")
@@ -72,7 +72,7 @@ public class ClienteController {
 
         if(bindingResult.hasFieldErrors("nacimiento") || bindingResult.hasFieldErrors("direccion") ||
                 bindingResult.hasFieldErrors("celular") || nacimientoHasErrors){
-            return "/cliente/miperfil";
+            return "/miperfil";
         }else{
             cliente.setDireccion(cliente.getDireccion().trim());
             Cliente clienteSes = (Cliente) session.getAttribute("usuario");
@@ -80,7 +80,7 @@ public class ClienteController {
             Cliente clienteDB = clienteRepository.findByCorreo(clienteSes.getCorreo());
             session.setAttribute("usuario", clienteDB);
             attr.addFlashAttribute("msg", "Se ha actualizado la información");
-            return "redirect:/cliente/miperfil";
+            return "redirect:/miperfil";
         }
     }
 
@@ -92,31 +92,31 @@ public class ClienteController {
         if (foto.isEmpty()) {
             attr.addFlashAttribute("error",1);
             attr.addFlashAttribute("msg", "Debe subir un archivo");
-            return "redirect:/cliente/miperfil";
+            return "redirect:/miperfil";
         }
         if (!verificarFoto(foto)) {
             attr.addFlashAttribute("error",1);
             attr.addFlashAttribute("msg", "Debe subir una imagen, no se acepta otros archivos");
-            return "redirect:/cliente/miperfil";
+            return "redirect:/miperfil";
         }
         String fotoNombre = foto.getOriginalFilename();
 
         if (fotoNombre.contains("..")) {
             attr.addFlashAttribute("error",1);
             attr.addFlashAttribute("msg", "No se permiten '..' en el archivo");
-            return "redirect:/cliente/miperfil";
+            return "redirect:/miperfil";
         }
 
         try {
             byte[] fotoB = foto.getBytes();
             clienteRepository.updateFoto(fotoB,clienteSes.getId());
             attr.addFlashAttribute("msg","Se ha actualizado la foto de perfil");
-            return "redirect:/cliente/miperfil";
+            return "redirect:/miperfil";
         } catch (IOException e){
             e.printStackTrace();
             attr.addFlashAttribute("error",1);
             attr.addFlashAttribute("msg", "Hubo un error al cargar el archivo");
-            return "redirect:/cliente/miperfil";
+            return "redirect:/miperfil";
         }
 
     }
