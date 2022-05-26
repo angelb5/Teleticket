01bc32f4c8543a20c4b36bec878f87da655be49c
Sergio <sergio.sotomayor@pucp.edu.pe>
2022-05-26 07:39:26 -0500

    form personas, minor changes.

--- src/main/java/pe/edu/pucp/teleticket/entity/Persona.java
+++ src/main/java/pe/edu/pucp/teleticket/entity/Persona.java
@@ -2,24 +2,32 @@ package pe.edu.pucp.teleticket.entity;
 
 import lombok.Getter;
 import lombok.Setter;
+import org.hibernate.validator.constraints.Length;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
 
 @Getter
 @Setter
 @Entity
 @Table(name = "personas")
 public class Persona {
+
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "idpersonas", nullable = false)
     private int id;
 
+    @NotEmpty(message = "Tiene que llenar el nombre")
+    @Length(message = "No puede exceder de los 70 caracteres", max = 70)
     @Column(name = "nombre", nullable = false, length = 45)
     private String nombre;
 
     @Column(name = "foto", nullable = false)
     private byte[] foto;
 
+    @NotNull(message = "Tiene que seleccionar un estado")
     @Lob
     @Column(name = "estado", nullable = false)
     private String estado;
