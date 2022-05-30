537fb28b7396e28081d8ad4c6c567536a2d5b0a4
Angel <a20190192@pucp.edu.pe>
2022-05-30 15:24:25 -0500

    Correccion obras controller

--- src/main/java/pe/edu/pucp/teleticket/controller/ClienteObrasController.java
+++ src/main/java/pe/edu/pucp/teleticket/controller/ClienteObrasController.java
@@ -6,6 +6,11 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import pe.edu.pucp.teleticket.dto.FuncionesCompra;
+import pe.edu.pucp.teleticket.dto.SedeFiltro;
+import pe.edu.pucp.teleticket.dto.SedesCompra;
+import pe.edu.pucp.teleticket.entity.Funcion;
+import pe.edu.pucp.teleticket.entity.Historial;
 import pe.edu.pucp.teleticket.entity.Obra;
 import pe.edu.pucp.teleticket.repository.CalificacionObraRepository;
 import pe.edu.pucp.teleticket.repository.FotoObraRepository;
@@ -34,8 +39,8 @@ public class ClienteObrasController {
     CalificacionObraRepository calificacionObraRepository;
 
     @GetMapping("/{idPath}")
-    public String gestionSede(Model model, @PathVariable("idPath") String idPath, @RequestParam("fecha") Optional<String> optfecha) {
-       /* int id=0;
+    public String gestionSede(Model model, @PathVariable("idPath") String idPath, @RequestParam("fecha") Optional<String> optfecha){
+        int id=0;
         LocalDate fecha;
         try{
             id= Integer.parseInt(idPath);
@@ -69,14 +74,9 @@ public class ClienteObrasController {
         model.addAttribute("sedes", sedesCompraList);
         model.addAttribute("obra", optionalObra.get());
         model.addAttribute("fotos", fotoObraRepository.findAllIdByIdObras(id));
-
-        // para las calificaciones, pensaba hacerlo aparte pero se puede integrar acá
         model.addAttribute("estrellas", calificacionObraRepository.calificaciones);
 
         return "/cliente/obras/obra";
-    }
-*/
-        return"/cliente/obras/obra";
     }
 
 }
