bc2e34fe72a272fd2cfbe4de62a2dd7360baa691
Angel <a20190192@pucp.edu.pe>
2022-06-06 19:19:20 -0500

    test java localdatetime

--- src/main/java/pe/edu/pucp/teleticket/controller/HomeController.java
+++ src/main/java/pe/edu/pucp/teleticket/controller/HomeController.java
@@ -3,14 +3,16 @@ package pe.edu.pucp.teleticket.controller;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import javax.servlet.http.HttpSession;
+import java.time.LocalDateTime;
 
 @Controller
 public class HomeController {
     @GetMapping({"/",""})
-    public String mostrarIndex(Authentication auth){
+    public String mostrarIndex(Authentication auth, Model model){
         String rol = "";
         if(auth!=null) {
             for (GrantedAuthority role : auth.getAuthorities()) {
@@ -24,7 +26,7 @@ public class HomeController {
         if (rol.equals("operador")) {
             return "redirect:/operador";
         }
-
+        model.addAttribute("ldt", LocalDateTime.now());
         return "index";
     }
 }
