ee9648bd46c86678a6f43e83761b88afd03bcb2d
Angel <a20190192@pucp.edu.pe>
2022-06-19 02:50:58 -0500

    Avance estadísticas

--- src/main/java/pe/edu/pucp/teleticket/controller/OperadorEstadisticasController.java
+++ src/main/java/pe/edu/pucp/teleticket/controller/OperadorEstadisticasController.java
@@ -1,18 +1,28 @@
 package pe.edu.pucp.teleticket.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import pe.edu.pucp.teleticket.dto.ObraEstadisticas;
 import pe.edu.pucp.teleticket.dto.PersonasListado;
 import pe.edu.pucp.teleticket.repository.*;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Optional;
 
 @Controller
 @RequestMapping("/operador/estadisticas")
 public class OperadorEstadisticasController {
+
+    @Value("${aplication.domain}")
+    private String DOMINIO;
 
     @Autowired
     CalificacionPersonaRepository calificacionPersonaRepository;
@@ -46,7 +56,30 @@ public class OperadorEstadisticasController {
         model.addAttribute("totalTickets",totalTickets);
         model.addAttribute("ventas",venta);
         model.addAttribute("totalSedes", totalSedes);
+        model.addAttribute("dominio", DOMINIO);
         return "operador/estadisticas/estadisticas";
+    }
 
+    @ResponseBody
+    @GetMapping("/obra/{id}")
+    public ResponseEntity<HashMap<String, Object>> obtenerProductoPorId(@PathVariable("id") String idStr) {
+        HashMap<String, Object> responseJson = new HashMap<>();
+        try {
+            int id = Integer.parseInt(idStr);
+            ObraEstadisticas obraEstadisticas = new ObraEstadisticas();
+            obraEstadisticas.setId(id);
+            obraEstadisticas.setFuncionMasVista("placeholder");
+            if (true) {
+                responseJson.put("result", "success");
+                responseJson.put("obra", obraEstadisticas);
+                return ResponseEntity.ok(responseJson);
+            } else {
+                responseJson.put("msg", "Obra no encontrada");
+            }
+        } catch (NumberFormatException e) {
+            responseJson.put("msg", "el ID debe ser un número entero positivo");
+        }
+        responseJson.put("result", "failure");
+        return ResponseEntity.badRequest().body(responseJson);
     }
 }
---
+++ src/main/java/pe/edu/pucp/teleticket/dto/ObraEstadisticas.java
@@ -0,0 +1,16 @@
+package pe.edu.pucp.teleticket.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class ObraEstadisticas {
+    int id;
+    String funcionMasVista;
+
+}
